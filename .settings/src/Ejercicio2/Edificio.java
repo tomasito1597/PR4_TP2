d097e30920dec2b8da2f9ff716d2394766efa35c
juancruz0077 <juanchievilbull@gmail.com>
2025-09-03 01:13:21 -0300

    Constructor y interface

--- .settings/src/Ejercicio2/Edificio.java
+++ .settings/src/Ejercicio2/Edificio.java
@@ -1,5 +1,10 @@
 package Ejercicio2;
 
 public class Edificio {
+	private double superficie;
+	
+	public Edificio(double superficie) {
+		this.superficie = superficie;
+	}
 
 }
--- .settings/src/Ejercicio2/IinstalacionesDeportivas.java
+++ .settings/src/Ejercicio2/IinstalacionesDeportivas.java
@@ -1,5 +1,5 @@
 package Ejercicio2;
 
 public interface IinstalacionesDeportivas {
-
+	int getTipoDeInstalacion(); // Ejemplo: 1 = cerrado, 2 = abierto
 }
