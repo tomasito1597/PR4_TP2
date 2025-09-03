ba8a4ba3bba6028f5315941f908a11273516e29b
FrancoMonzon <francodavidmonzon@hotmail.com>
2025-09-03 02:25:09 -0300

    Agregando clase Polideportivo

---
+++ .settings/src/Ejercicio2/Polideportivo.java
@@ -0,0 +1,14 @@
+package Ejercicio2;
+
+class Polideportivo extends Edificio {
+	
+    public Polideportivo(double superficie) {
+    	
+		super(superficie);
+		
+	}
+    
+	private String nombre;
+    private int tipoDeInstalacion;
+
+}
